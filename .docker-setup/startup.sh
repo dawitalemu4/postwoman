ca11a585cbe3a5197d4be95ba8fc971d63a3932d
Dawit Alemu <dndawita@gmail.com>
2024-04-23 16:01:50 -0400

    finished docker startup script

--- .docker-setup/startup.sh
+++ .docker-setup/startup.sh
@@ -9,39 +9,33 @@ cd D:/developer/postwoman
 # start up the docker engine 
 
 # for windows, simply put your path to docker desktop, the default is the following:
-
 "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
 # for mac
-
 # open -a Docker
 
 
 # wait for docker to start
 
 # for windows
-
 while :
 do
 
-    if ! docker info | grep -q "ERROR"
+    if ! docker info 2>&1 | grep -q "ERROR"
     then
         break
+    else
+        sleep 1
     fi
-
-    sleep 1
 done
 
 # for mac
-
 # while [ ! "$(ps aux | grep -v grep | grep -c $PROCESS)" -gt 0 ]
 # do
 #     sleep 1
 # done
 
-echo "Docker"
-
 
 # start up postwoman
 
-# docker-compose up
+docker-compose up
